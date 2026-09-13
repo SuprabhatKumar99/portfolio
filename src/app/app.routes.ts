dc7f4bbdcd27ed4f5dbcc4538a7c58aec038bbc3
Suprabhat <supra.blast45@gmail.com>
2026-09-13 14:25:22 +0530

    readme update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,41 +4,41 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    title: 'Home | Suprabhat Banerjee'
+    title: 'Home | Suprabhat Kumar S.C.B'
   },
   {
     path: 'about',
     loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
-    title: 'About | Suprabhat Banerjee'
+    title: 'About | Suprabhat Kumar S.C.B'
   },
   {
     path: 'skills',
     loadComponent: () => import('./pages/skills/skills.component').then(m => m.SkillsComponent),
-    title: 'Skills | Suprabhat Banerjee'
+    title: 'Skills | Suprabhat Kumar S.C.B'
   },
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
-    title: 'Projects | Suprabhat Banerjee'
+    title: 'Projects | Suprabhat Kumar S.C.B'
   },
   {
     path: 'projects/:slug',
     loadComponent: () => import('./pages/project-detail/project-detail.component').then(m => m.ProjectDetailComponent),
-    title: 'Project Case Study | Suprabhat Banerjee'
+    title: 'Project Case Study | Suprabhat Kumar S.C.B'
   },
   {
     path: 'experience',
     loadComponent: () => import('./pages/experience/experience.component').then(m => m.ExperienceComponent),
-    title: 'Experience & Education | Suprabhat Banerjee'
+    title: 'Experience & Education | Suprabhat Kumar S.C.B'
   },
   {
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent),
-    title: 'Contact | Suprabhat Banerjee'
+    title: 'Contact | Suprabhat Kumar S.C.B'
   },
   {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent),
-    title: '404 - Not Found | Suprabhat Banerjee'
+    title: '404 - Not Found | Suprabhat Kumar S.C.B'
   }
 ];
